c2d51c871f7a20c670a1963b9e0405e855407bee
grant <grant@grant-miller.com>
2022-07-06 10:22:51 -0400

    init commit

--- tracker.py
+++ tracker.py
@@ -1,16 +1,14 @@
 from flask import render_template
 from flask_login_dictabase_blueprint import VerifyLogin
 from flask_login_dictabase_blueprint.menu import GetMenu
-import config
-
 
 BUTTONS = [
-        {'id': 0, 'text': 'Had Sex'},
-        {'id': 1, 'text': 'Had Bleeding'},
-        {'id': 2, 'text': 'Had Cramping'},
-        {'id': 3, 'text': 'Positive Pregnancy Test'},
-        {'id': 4, 'text': 'Negative Pregnancy Test'},
-    ]
+    {'id': 0, 'text': 'Had Sex'},
+    {'id': 1, 'text': 'Had Bleeding'},
+    {'id': 2, 'text': 'Had Cramping'},
+    {'id': 3, 'text': 'Positive Pregnancy Test'},
+    {'id': 4, 'text': 'Negative Pregnancy Test'},
+]
 
 
 def setup(app):
